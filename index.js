c59fb32e8469c24288d1047cdd3cbc55480a21f7
Cheese <>
2021-04-30 00:53:43 +0000

    uwu

--- index.js
+++ index.js
@@ -3,18 +3,23 @@ var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var cryptoRandomString = require('crypto-random-string');
-var fflate = require('fflate');
+var btoa = require('btoa');
 var SAT = require('sat');
 var fastnoise = require('fastnoisejs');
+var sizeof = require('object-sizeof')
+var LZUTF8 = require('lzutf8');
+
 var V = SAT.Vector;
 
 const noise = fastnoise.Create(Math.floor(Math.random() * 10))
-noise.SetNoiseType(fastnoise.Simplex)
+noise.SetNoiseType(fastnoise.Cellular)
 
 const lerp = (x, y, a) => x * (1 - a) + y * a;
 
 var players = [];
-var objects = require('./worlds/world1').start(SAT);
+var objects = require('./worlds/world1').start(SAT, noise);
+
+console.log(objects.length)
 
 var prevss = [];
 
@@ -30,85 +35,125 @@ http.listen(3000, () => {
 
 setInterval(function() {
   for (let a = 0; a < players.length; a++) {
-    // prevss[a] = {};
-    // prevss[a].x = players[a].pos.x;
-    // prevss[a].y = players[a].pos.y;
+    if (players[a]) {
+      // prevss[a] = {};
+      // prevss[a].x = players[a].pos.x;
+      // prevss[a].y = players[a].pos.y;
 
-    if (!players[a].pspeed) {
-      players[a].pspeed = 2.5;
-    }
-
-    if (!players[a].dash) {
-      players[a].dash = {
-        cooldown: 0,
-        duration: 0
-      };
-    }
-
-    if (players[a].keyq.space === true) {
-      if (players[a].dash.cooldown <= 0) {
-        players[a].dash.cooldown = 96;
-        players[a].dash.duration = 16;
-      }
-    };
-
-    if (players[a].dash.duration >= 0) {
-      players[a].dash.duration--;
-      players[a].pspeed += 1;
-      if (players[a].dash.duration === 0) {
+      if (!players[a].pspeed) {
         players[a].pspeed = 2.5;
       }
-    }
 
-    players[a].dash.cooldown--;
-
-    if (players[a].keyq.w === true) {
-      players[a].pos.y -= players[a].pspeed;
-    };
-
-    if (players[a].keyq.a === true) {
-      players[a].pos.x -= players[a].pspeed;
-    };
-
-    if (players[a].keyq.s === true) {
-      players[a].pos.y += players[a].pspeed;
-    };
-
-    if (players[a].keyq.d === true) {
-      players[a].pos.x += players[a].pspeed;
-    };
-
-    for (let o = 0; o < objects.length; o++) {
-      let response = new SAT.Response();
-      let collided = SAT.testCirclePolygon(players[a], objects[o], response);
-      if (collided) {
-        let overlapV = response.overlapV.clone().scale(-1);
-        players[a].pos.x += overlapV.x;
-        players[a].pos.y += overlapV.y;
+      if (!players[a].dash) {
+        players[a].dash = {
+          cooldown: 0,
+          duration: 0
+        };
       }
-    }
 
-    let pss = [];
-    for (let b = 0; b < players.length; b++) {
-      let ap = players[a].pos.x - players[b].pos.x;
-      let bp = players[a].pos.y - players[b].pos.y;
-      let c = Math.sqrt(ap * ap + bp * bp);
-      if (c < 700) {
-        pss[b] = { pos: players[b].pos };
+      if (players[a].keyq.space === true) {
+        if (players[a].dash.cooldown <= 0) {
+          players[a].dash.cooldown = 96;
+          players[a].dash.duration = 16;
+        }
+      };
+
+      if (players[a].dash.duration >= 0) {
+        players[a].dash.duration--;
+        players[a].pspeed += 1;
+        if (players[a].dash.duration === 0) {
+          players[a].pspeed = 2.5;
+        }
       }
-    }
 
-    let oss = [];
-    for (let o = 0; o < objects.length; o++) {
-      let ap = players[a].pos.x - objects[o].pos.x;
-      let bp = players[a].pos.y - objects[o].pos.y;
-      let c = Math.sqrt(ap * ap + bp * bp);
-      if (c < 700) {
-        oss[o] = { pos: objects[o].pos };
+      players[a].dash.cooldown--;
+
+      if (players[a].keyq.w === true) {
+        players[a].pos.y -= players[a].pspeed;
+      };
+
+      if (players[a].keyq.a === true) {
+        players[a].pos.x -= players[a].pspeed;
+      };
+
+      if (players[a].keyq.s === true) {
+        players[a].pos.y += players[a].pspeed;
+      };
+
+      if (players[a].keyq.d === true) {
+        players[a].pos.x += players[a].pspeed;
+      };
+
+      for (let o = 0; o < objects.length; o++) {
+        let response = new SAT.Response();
+        let collided = SAT.testCirclePolygon(players[a], objects[o], response);
+        if (collided) {
+          let overlapV = response.overlapV.clone().scale(-1);
+          players[a].pos.x += overlapV.x;
+          players[a].pos.y += overlapV.y;
+          if (objects[o].tt === 2) {
+            players[a].pos.x = 100;
+            players[a].pos.y = 100;
+          }
+        }
       }
-    }
 
-    io.to(players[a].sid).emit('t', pss, oss);
+      let pss = [];
+      for (let b = 0; b < players.length; b++) {
+        if (players[b]) {
+          let ap = players[a].pos.x - players[b].pos.x;
+          let bp = players[a].pos.y - players[b].pos.y;
+          let c = Math.sqrt(ap * ap + bp * bp);
+          if (c < 700) {
+            pss[b] = { pos: players[b].pos, name: players[b].name };
+          }
+        }
+      }
+
+      let oss = [];
+      if (!players[a].objloaded) {
+        for (let o = 0; o < objects.length; o++) {
+          let ap = players[a].pos.x - objects[o].pos.x;
+          let bp = players[a].pos.y - objects[o].pos.y;
+          let c = Math.sqrt(ap * ap + bp * bp);
+          if (c < 700) {
+            oss[o] = { pos: objects[o].pos, tt: objects[o].tt };
+          }
+        }
+      }
+
+      if (!players[a].ocooldown) {
+        players[a].ocooldown = 0;
+      }
+
+      players[a].ocooldown--;
+
+      let pp = new Promise((resolve, reject) => {
+        LZUTF8.compressAsync(JSON.stringify(pss), { outputEncoding: "ByteArray" }, r => {
+          resolve(r);
+          return;
+        });
+      });
+
+      let op = new Promise((resolve, reject) => {
+        LZUTF8.compressAsync(JSON.stringify(oss), { outputEncoding: "ByteArray" }, r => {
+          resolve(r);
+          return;
+        });
+      });
+
+      if (players[a].ocooldown <= 0) {
+        Promise.all([pp, op]).then(([rpp, rop]) => {
+          io.to(players[a].sid).emit('t', rpp, rop);
+          players[a].ocooldown = 32;
+        });
+      } else {
+        pp.then(rpp => {
+          io.to(players[a].sid).emit('t', rpp);
+        });
+      }
+
+    }
   }
 }, 1000 / 64);
 
@@ -117,13 +162,20 @@ io.on('connection', (socket) => {
 
   //spawn
   var id = players.length;
-  players.push(new SAT.Circle(new V(100, 100), 20));
+  players.push(new SAT.Circle(new V(5 * 40, 5 * 40), 20));
   socket.emit('id', id);
 
   players[id].sid = socket.id;
 
   socket.on('disconnect', () => {
     console.log('user disconnected');
+    players[id] = null;
+  });
+
+  socket.on('name', (name) => {
+    if (!players[id].name) {
+      players[id].name = name;
+    }
   });
 
   //keys
--- public/script.js
+++ public/script.js
@@ -9,6 +9,21 @@ var plast = [];
 
 var lastt;
 
+var temptile;
+
+var V = SAT.Vector;
+var clientp = new SAT.Circle(new V(100, 100), 20);
+
+var name = prompt("Username:", "");
+
+if (name != null) {
+  socket.emit('name', name)
+}
+
+function preload() {
+  temptile = loadImage('assets/temptile.png');
+}
+
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
   rectMode(CENTER);
@@ -27,25 +42,41 @@ function draw() {
     if (plist[i] && plast[i]) {
       push();
       translate((lerp(plast[i].pos.x, plist[i].pos.x, lasttd) - 20) * (window.innerWidth / 1000), (lerp(plast[i].pos.y, plist[i].pos.y, lasttd) - 20) * (window.innerWidth / 1000));
-      // let angleb = atan2((mouseY - 20) - windowHeight / 2, (mouseX - 20) - windowWidth / 2);
-      // if (i === id) {
-      //   rotate(angleb)
-      // }
-      // line(0, 0, 20 * (window.innerWidth / 500), 0 * (window.innerWidth / 500));
       circle(0, 0, 20 * (window.innerWidth / 500));
+      textSize(16 * (window.innerWidth / 1000));
+      textAlign(CENTER);
+      text(plist[i].name, 0, -20 * (window.innerWidth / 1000));
       pop();
     }
   }
+  // push();
+  // translate((clientp.pos.x - 20) * (window.innerWidth / 1000), (clientp.pos.y - 20) * (window.innerWidth / 1000));
+  // fill(color('purple'));
+  // circle(0, 0, 20 * (window.innerWidth / 500));
+  // pop();
   for (let o = 0; o < olist.length; o++) {
     if (olist[o]) {
-      push();
-      fill(color('black'));
-      translate(olist[o].pos.x * (window.innerWidth / 1000), olist[o].pos.y * (window.innerWidth / 1000))
-      rect(0, 0, 20 * (window.innerWidth / 500), 20 * (window.innerWidth / 500));
-      pop();
+      let ap = plist[id].pos.x - olist[o].pos.x;
+      let bp = plist[id].pos.y - olist[o].pos.y;
+      let c = Math.sqrt(ap * ap + bp * bp);
+      if (c < 700) {
+        push();
+        translate(olist[o].pos.x * (window.innerWidth / 1000), olist[o].pos.y * (window.innerWidth / 1000));
+        if (olist[o].tt === 1) {
+          image(temptile, -10 * (window.innerWidth / 500), -10 * (window.innerWidth / 500), 20 * (window.innerWidth / 500), 20 * (window.innerWidth / 500));
+        }
+        if (olist[o].tt === 2) {
+          fill(color('red'));
+          rect(0, 0, 20 * (window.innerWidth / 500), 20 * (window.innerWidth / 500));
+        }
+        pop();
+      }
     }
   }
 }
+
+var pspeed = 2.5;
+
 
 function keyPressed() {
   if (keyCode === 87) {
@@ -54,6 +85,7 @@ function keyPressed() {
 
   if (keyCode === 65) {
     socket.emit('a', true)
+
   }
 
   if (keyCode === 83) {
@@ -62,6 +94,7 @@ function keyPressed() {
 
   if (keyCode === 68) {
     socket.emit('d', true)
+
   }
 
   if (keyCode === 32) {
@@ -98,8 +131,10 @@ function windowResized() {
 socket.on('t', (pnow, onow) => {
   lastt = performance.now();
   plast = plist;
-  plist = pnow;
-  olist = onow;
+  plist = JSON.parse(LZUTF8.decompress(new Uint8Array(pnow)));
+  if (onow) {
+    olist = JSON.parse(LZUTF8.decompress(new Uint8Array(onow)));
+  }
 });
 
 socket.on('id', (sid) => {
